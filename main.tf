70b604c689475de56f325ab0c294aa7e0ffe29c2
Gustavo Oliveira <9us7av0@users.noreply.github.com>
2023-01-10 20:52:31 -0500

    autoscaling

--- main.tf
+++ main.tf
@@ -29,24 +29,27 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.7.0"
 
-  ami                    = data.aws_ami.app_ami.id
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  vpc_security_groups = [module.blog_sg.security_group_id]
+
+  image_id               = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id              = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
 module "blog-alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
-  name = "my-alb"
+  name = "blog_alb"
 
   load_balancer_type = "application"
 
